6558d4b46525fd6a88ed7e7ff21e2fd3e219408e
Kushaan Gowda <kushaangowda@yahoo.com>
2020-11-11 15:48:39 +0530

    added files

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+	return (
+		<div className="song-list">
+			<ul>
+				<li>this wild darkness 1</li>
+				<li>this wild darkness 2</li>
+				<li>this wild darkness 3</li>
+			</ul>
+		</div>
+	);
+};
+
+export default SongList;
